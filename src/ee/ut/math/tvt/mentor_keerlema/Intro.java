d477df5d7d42d6891bda55c04e2a2a611e95e310
markotaht <markotaht@gmail.com>
2014-10-08 15:25:43 +0300

    InitUI ja Init classes

--- src/ee/ut/math/tvt/mentor_keerlema/Intro.java
+++ src/ee/ut/math/tvt/mentor_keerlema/Intro.java
@@ -1,5 +1,24 @@
 package ee.ut.math.tvt.mentor_keerlema;
 
-public class Intro {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+public class Intro extends Application {
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage primary){
+		primary.setTitle("IntroUI");
+		IntroUI ui = new IntroUI();
+		
+		GridPane root;
+		root = ui.UI();
+		
+		primary.setScene(new Scene(root, 670,450));
+		primary.show();
+	}
 }
--- src/ee/ut/math/tvt/mentor_keerlema/IntroUI.java
+++ src/ee/ut/math/tvt/mentor_keerlema/IntroUI.java
@@ -1,52 +1,63 @@
 package ee.ut.math.tvt.mentor_keerlema;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
-import com.jgoodies.looks.windows.WindowsLookAndFeel;
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
-public class IntroUI extends JFrame{
+public class IntroUI{
 	
-	private static final long serialVersionUID = 1L;
-
 	public IntroUI(){
-		setTitle("IntroUI");
-		
-	    try {
-	        UIManager.setLookAndFeel(new WindowsLookAndFeel());
-	      } catch (UnsupportedLookAndFeelException e1) {
-	        e1.printStackTrace();
-	      }
-	    
-	    draw();
-	    
-	    int width = 600;
-	    int height = 400;
-	    setSize(width, height);
-	    Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-	    setLocation((screen.width - width) / 2, (screen.height - height) / 2);
+	}
 
-	    addWindowListener(new WindowAdapter() {
-	      @Override
-	      public void windowClosing(WindowEvent e) {
-	        System.exit(0);
-	      }
-	    });
+	public GridPane UI(){
+		GridPane root = new GridPane();
+		root.setHgap(10);
+		root.setVgap(10);
+		root.setPadding(new Insets(25,25,25,25));
+		
+		String Version = "version.properties";
+		Properties prop = null;
+		try {
+			prop = getProperties(Version);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Text tname = new Text("Team name: Mentor Keerlema");
+		Text tleader = new Text("Team leader: Hendrik Rasmus Elmet");
+		Text tleaderemail = new Text("Team leader email: ");
+		Text tmembers = new Text("Team memers: Marko Täht, Rene Lehtma");
+		Text logo = new Text("Team logo:");
+		Image Logo = new Image("logo.jpg");
+		ImageView iv = new ImageView();
+		iv.setImage(Logo);
+		Text version = new Text("Software version: " + prop.getProperty("build.number"));
+		root.add(tname, 0,0);
+		root.add(tleader, 0, 1);
+		root.add(tleaderemail, 0, 2);
+		root.add(tmembers, 0, 3);
+		root.add(logo, 0, 4);
+		root.add(iv,1,4);
+		root.add(version, 0, 5);
+		return root;
 	}
 	
-
-	
-	private void draw(){
-		JPanel panel = new JPanel();
+	public Properties getProperties(String name) throws IOException{
+		Properties prop = new Properties();
 		
-		
+		InputStream input = getClass().getClassLoader().getResourceAsStream(name);
+		if(input == null){
+			throw new FileNotFoundException("property file '" + name + "' not found!");
+		}
+		prop.load(input);		
+		return prop;
 	}
 }
