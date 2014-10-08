efa85820ae7f02f79024955423fbc78c3fe47a44
markotaht <markotaht@gmail.com>
2014-10-08 14:09:34 +0300

    Lisa jubinad

--- src/ee/ut/math/tvt/mentor_keerlema/IntroUI.java
+++ src/ee/ut/math/tvt/mentor_keerlema/IntroUI.java
@@ -1,5 +1,52 @@
 package ee.ut.math.tvt.mentor_keerlema;
 
-public class IntroUI {
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.jgoodies.looks.windows.WindowsLookAndFeel;
+
+public class IntroUI extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+
+	public IntroUI(){
+		setTitle("IntroUI");
+		
+	    try {
+	        UIManager.setLookAndFeel(new WindowsLookAndFeel());
+	      } catch (UnsupportedLookAndFeelException e1) {
+	        e1.printStackTrace();
+	      }
+	    
+	    draw();
+	    
+	    int width = 600;
+	    int height = 400;
+	    setSize(width, height);
+	    Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+	    setLocation((screen.width - width) / 2, (screen.height - height) / 2);
+
+	    addWindowListener(new WindowAdapter() {
+	      @Override
+	      public void windowClosing(WindowEvent e) {
+	        System.exit(0);
+	      }
+	    });
+	}
+	
+
+	
+	private void draw(){
+		JPanel panel = new JPanel();
+		
+		
+	}
 }
