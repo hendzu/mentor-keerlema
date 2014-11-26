fa6f22f2651c73679cc377914a0d3440bfc8cfa6
Rene <Rene@fine-bartender>
2014-11-26 02:58:19 +0200

    Removed the unneccessary stuff that I added previously.

--- src/ee/ut/math/tvt/salessystem/ui/model/SalesSystemTableModel.java
+++ src/ee/ut/math/tvt/salessystem/ui/model/SalesSystemTableModel.java
@@ -1,25 +1,23 @@
 package ee.ut.math.tvt.salessystem.ui.model;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import javax.swing.table.AbstractTableModel;
 
 import ee.ut.math.tvt.salessystem.domain.data.DisplayableItem;
 
 /**
  * Generic table model implementation suitable for extending.
  */
-public abstract class SalesSystemTableModel<T extends DisplayableItem> extends 
-AbstractSalesSystemTableModel<T> {
+public abstract class SalesSystemTableModel<T extends DisplayableItem> extends AbstractTableModel 
+{
 
     private static final long serialVersionUID = 1L;
-
-   // protected List<T> rows;
+    protected final String[] headers;
 
     public SalesSystemTableModel(final String[] headers) {
-    	super(headers);
-     //   rows = new ArrayList<T>();
+    	this.headers = headers;
     }
 
     /**
@@ -36,18 +34,15 @@ AbstractSalesSystemTableModel<T> {
     }
 
     public int getRowCount() {
-        //return rows.size();
     	return getTableRows().size();
     }
 
     public Object getValueAt(final int rowIndex, final int columnIndex) {
-        //return getColumnValue(rows.get(rowIndex), columnIndex);
         return getColumnValue(getTableRows().get(rowIndex), columnIndex);
     }
 
     // search for item with the specified id
     public T getItemById(final long id) {
-       // for (final T item : rows) 
     	for (final T item : getTableRows()){
             if (item.getId() == id)
                 return item;
@@ -56,36 +51,27 @@ AbstractSalesSystemTableModel<T> {
     }
 
     public abstract List<T> getTableRows();
-    //{
-     //   return rows;
-    //}
     
     public void clear(){
     	getTableRows().clear();
     	fireTableDataChanged();
     }
-//    public void clear() {
- //       rows = new ArrayList<T>();
-  //      fireTableDataChanged();
-    //}
 
     public void populateWithData(final List<T> data)
     {
-   //     rows.clear();
-     //   rows.addAll(data);
         getTableRows().clear();
         getTableRows().addAll(data);
     }
     
     public void addRow(T row)
     {
-        //rows.add(row);
     	getTableRows().add(row);
         fireTableDataChanged();
     }
     
     public T getRow(int index) {
-       // return rows.get(index);
     	return getTableRows().get(index);
     }
 }
+
+
