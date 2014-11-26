411ca8d54ae59ee1a7f6456013308b8fe43ffb8b
markotaht <markotaht@gmail.com>
2014-11-26 22:09:47 +0200

    Added task2 to the main branch. programs can only be run through ANT

---
+++ Task2/src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
@@ -0,0 +1,131 @@
+package ee.ut.math.tvt.salessystem.domain.controller.impl;
+
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.data.Client;
+import ee.ut.math.tvt.salessystem.domain.data.Sale;
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
+import ee.ut.math.tvt.salessystem.util.HibernateUtil;
+import java.util.List;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ * Implementation of the sales domain controller.
+ */
+public class SalesDomainControllerImpl implements SalesDomainController {
+
+    private static final Logger log = Logger.getLogger(SalesDomainControllerImpl.class);
+
+    private SalesSystemModel model;
+
+    private Session session = HibernateUtil.currentSession();
+
+    @Override
+	@SuppressWarnings("unchecked")
+    public List<StockItem> getAllStockItems() {
+        List<StockItem> result =
+            session
+                .createQuery("from StockItem")
+                .list();
+
+        log.info(result.size() + " items loaded from disk");
+
+        return result;
+    }
+
+    @Override
+	@SuppressWarnings("unchecked")
+    public List<Sale> getAllSales() {
+        List<Sale> result = session.createQuery("from Sale").list();
+        log.info(result.size() + " Sales loaded from disk");
+
+        return result;
+    }
+
+
+    @Override
+	@SuppressWarnings("unchecked")
+    public List<Client> getAllClients() {
+        List<Client> clients =
+            session.createQuery("from Client").list();
+
+        log.info(clients.size() + " clients loaded from disk");
+
+        return clients;
+    }
+
+    @Override
+	public Client getClient(long id) {
+        return (Client) session.get(Client.class, id);
+    }
+
+
+    private StockItem getStockItem(long id) {
+        return (StockItem) session.get(StockItem.class, id);
+    }
+
+    @Override
+	public void registerSale(Sale sale)
+    {
+        // Begin transaction
+        Transaction tx = session.beginTransaction();
+     // Reduce quantities of stockItems in warehouse
+        System.out.println(sale.getId());
+        for (SoldItem item : sale.getSoldItems()) {
+            // Associate with current sale
+            item.setSale(sale);
+            System.out.println(item.getStockItem().getId());
+            StockItem stockItem = getStockItem(item.getStockItem().getId());
+            stockItem.setQuantity(stockItem.getQuantity() - item.getQuantity());
+            System.out.println(stockItem);
+            session.save(stockItem);
+        }
+        
+        session.save(sale);
+
+        // end transaction
+        tx.commit();
+        model.getPurchaseHistoryTableModel().addRow(sale);
+    }
+
+
+    @Override
+	public void createStockItem(StockItem stockItem) {
+        // Begin transaction
+        Transaction tx = session.beginTransaction();
+        session.save(stockItem);
+        tx.commit();
+        model.getWarehouseTableModel().addRow(stockItem);
+        log.info("Added new stockItem : " + stockItem);
+    }
+
+
+    @Override
+	public void cancelCurrentPurchase() {
+        // XXX - Cancel current purchase
+        log.info("Current purchase canceled");
+    }
+
+    @Override
+	public void startNewPurchase() {
+        // XXX - Start new purchase
+        log.info("New purchase started");
+    }
+
+
+
+    @Override
+	public void setModel(SalesSystemModel model) {
+        this.model = model;
+    }
+
+
+    @Override
+    public void endSession() {
+        HibernateUtil.closeSession();
+    }
+
+}
